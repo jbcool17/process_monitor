4164605b23dbf65b731e55d837822ef07016e2b6
John Brilla <jbrilla17@gmail.com>
2017-02-28 17:21:40 -0500

    add params method

--- lib/app.rb
+++ lib/app.rb
@@ -12,16 +12,26 @@ class App < CustomFramework
       end
     when '/all'
       status = '200'
-      headers = {"Content-Type" => 'text/html'}
-      piece_of_advice = Advice.new.generate
+      headers = {"Content-Type" => 'application/json'}
+      search = 'ruby'
+      output = `ps $(pgrep -f #{search})`.split(/\n/)
+      ps_headers = output.slice!(0).split(' ')
+      processes_data = output.map { |pro| [pro.strip.split(' ').slice(0,4) , pro.strip.split(' ').slice(4)].join(' ').split(' ') }
+
       response(status, headers) do
-        erb :advice, message: piece_of_advice
+        json({status: 'This is json.', search: search, headers: ps_headers, processes: processes_data})
       end
     when '/status'
       status = '200'
       headers = {"Content-Type" => 'application/json'}
+      output = `ps aux 79691`.split(/\n/)
+
+      ps_headers = output.slice!(0).split(' ')
+      process = output.join(' ').gsub(/ +/, ' ').split(' ').slice!(0,10)
+      command = output.join(' ').gsub(/ +/, ' ').split(' ').slice(10..-1).join(' ')
+
       response(status, headers) do
-        json({animal: "dogs"})
+        json({ status: "dogs", headers: ps_headers, process: process, command: command, params: params(env['QUERY_STRING']) })
       end
     else
       status = '404'
--- lib/custom_framework.rb
+++ lib/custom_framework.rb
@@ -16,6 +16,16 @@ class CustomFramework
     data.to_json
   end
 
+  def params(query_string)
+    params = {}
+
+    query_string.split('&').each do |q|
+      params[q.split('=')[0]] = q.split('=')[1]
+    end
+
+    params
+  end
+
   def response(status, headers, body = '')
     body = yield if block_given?
     [status, headers, [body]]
